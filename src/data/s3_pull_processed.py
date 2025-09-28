42c94daf705276021dc5d29750a1d2a355d6d258
realtheai <realtheai.insight@gmail.com>
2025-09-28 16:44:10 +0900

    add model features

--- src/data/s3_pull_processed.py
+++ src/data/s3_pull_processed.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 from io import StringIO
 
-def get_s3_data():
+def get_processed_data():
     """S3에서 날씨 및 미세먼지 데이터를 가져와서 DataFrame으로 반환"""
     
     # S3 클라이언트 생성
@@ -19,7 +19,7 @@ def get_s3_data():
     # 날씨 데이터 가져오기
     weather_response = s3_client.get_object(
         Bucket=bucket_name, 
-        Key='ml_dataset/past_data/weather_pm10_integrated_full.csv'
+        Key='ml_dataset/weather_features_full.csv'
     )
     weather_content = weather_response['Body'].read().decode('utf-8')
     weather_df = pd.read_csv(StringIO(weather_content), low_memory=False)
